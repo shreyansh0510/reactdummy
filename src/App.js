b2ef8a44754a499a42b21e12bfa562a07fe13205
shreyansh <shreyanshsingh0510@gmail.com>
2024-04-21 12:31:29 +0530

    fixed UI issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>App</h1>
-      </header>
+    <div className="app">
+      <h1>App</h1>
     </div>
   );
 }
